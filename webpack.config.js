6956ae54576178126782f9d873998676443c98c6
new5tt <chn_614si@163.com>
2019-05-09 11:37:49 +0800

    修改

--- webpack.config.js
+++ webpack.config.js
@@ -99,8 +99,8 @@ module.exports = {
                 use: {
                         loader: 'file-loader',
                         options: {
-                            outputPath: 'fonts/',
-                            publicPath: "/",
+                            outputPath: './fonts/',
+                            publicPath: "/fonts/",
                             name: '[name].[ext]',//// hash值为7位，ext自动补全文件扩展名
                         }
                     }
