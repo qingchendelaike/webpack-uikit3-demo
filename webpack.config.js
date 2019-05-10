d6ac33ebb69f33469524fe4ff9ae14377acb30a7
new5tt <chn_614si@163.com>
2019-05-10 12:09:41 +0800

    不压缩html 配置

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
                 use: {
                     loader: 'html-loader',
                     options: {
-                        minimize: true,
+                        minimize: false,
                         attrs: [':data-src']
                     }
                 }
